319735c5668b676ec34ba990c552e6da295d77ce
TBooker <booker@zoology.ubc.ca>
2022-08-22 09:36:25 -0700

    edited gitignore

--- plot_BGS_results.R
+++ plot_BGS_results.R
@@ -10,14 +10,14 @@ pi_0 <- mean(bgs[bgs$Nes==0,]$window_1)
 bgs_m <- melt(bgs, id = c("Nes", "rep", "N", "r"))
 bgs_m$B <- bgs_m$value/pi_0
 
-bgs_m <- bgs_m[bgs_m$Nes!=0,]
+bgs_p <- bgs_m[bgs_m$Nes!=0,]
 
-bgs_m$labs <- factor(bgs_m$variable,
+bgs_p$labs <- factor(bgs_p$variable,
                      levels = c("window_1", "window_2"),
                      labels = c(expression(italic(r) > 0),
                                 expression(italic(r)*" = 0")))
 
-ggplot(data = bgs_m)+
+ggplot(data = bgs_p)+
   stat_summary(aes( x = Nes*2, y = B))+
   scale_x_log10(expression(italic( "2N"[e]*"s") ) )+
   scale_y_continuous(expression(italic(B)))+
